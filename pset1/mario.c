0da4491b872ee25a65484100ea69634ef94a905f
Aaron Wilkinson <aaronwilkinson@gmail.com>
2017-03-30 15:06:41 -0500

    Create mario.c

---
+++ pset1/mario.c
@@ -0,0 +1,42 @@
+// This is for the "more comfortable" version of the problem
+
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void){
+    
+    // initialize height variable
+    int height;
+    
+    // prompt user for height and repeat until valid number given
+    do{
+        printf("Height: ");
+        height = get_int();
+    }
+    while(height > 23 || height < 0);
+    
+    // construct pyramid
+    for(int row = 0; row < height; row++){
+
+        // add padding spaces to row
+        for(int i = 0; i < (height - row - 1); i++){
+            printf(" ");
+        }
+        
+        // add first pyramid's hashes to row
+        for(int i = 0; i <= row; i++){
+            printf("#");
+        }
+        
+        // add middle spaces to row
+        printf("  ");
+        
+        // add second pyramid's hashes to row
+        for(int i = 0; i <= row; i++){
+            printf("#");
+        }
+        
+        // terminate line with newline character
+        printf("\n");
+    }
+}
